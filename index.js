e45c812a82077ddf111e0cd500f805438b475e8a
Nightowlam <nightowl3.00am@gmail.com>
2023-01-17 20:21:48 +0600

    Add product env setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const adminRouter = require("./routes/admin");
@@ -6,11 +7,11 @@ const shopRouter = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRouter);
+app.use("/admin", adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h3>Page not found</h3>");
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Hello from Express");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
