b8b2b0abeedbf128629a71ea5b1f5cdf8ef175e2
Nightowlam <nightowl3.00am@gmail.com>
2023-01-18 13:59:29 +0600

    pug dynamic HTML implemented

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const adminRouter = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRouter);
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use("/admin", adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
